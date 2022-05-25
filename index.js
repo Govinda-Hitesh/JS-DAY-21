00adb992afdbb1a60c4c82c860c85e9be1991002
Govinda-Hitesh <102047553+Govinda-Hitesh@users.noreply.github.com>
2022-05-25 20:19:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+//type error,syntax error, ref. error
+// const value = ()>{
+
+// }
+
+
+//type error
+const b = 20;
+const c = 10;
+c=20;
+
+//
+
+let sym1 = symbol('cricket');
+let sym2 = symbol('cricket');
+console.log(sym1===sym2);
+
+
+//IIFE - Immediately Invoked Function Expressions - 
+(()=>{
+    const david = "David";
+
+})();
+
+const david = "David";
